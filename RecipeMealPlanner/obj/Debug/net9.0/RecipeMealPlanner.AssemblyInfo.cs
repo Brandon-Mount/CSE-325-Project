4d29952bb8d840fb33cb25a6a94f8ccb58b99b58
Brandon-Mount <bmount44@gmail.com>
2026-06-09 23:47:23 -0600

    worked on project

--- RecipeMealPlanner/obj/Debug/net9.0/RecipeMealPlanner.AssemblyInfo.cs
+++ RecipeMealPlanner/obj/Debug/net9.0/RecipeMealPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeMealPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2214beb8d977a7edfd2188ea2916ab3d8a22da9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e3f0a76f1845473c43989fe2bd740d4f73535d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeMealPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeMealPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
